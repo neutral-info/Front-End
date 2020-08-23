d189f6cc818a8ad43047acb421d52edff66050ea
阿洧 <a65162@gmail.com>
2020-08-23 17:12:44 +0800

    refactor: change the way to call news api feat: search Input style

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,63 @@
 <template>
-  <b-container>
-    <div class="d-flex justify-content-end align-items-center mb-2 px-5">
-      <div class="d-flex align-items-center search">
+  <b-container class="my-4">
+    <div class="searchForm">
+      <div
+        :class="{
+          'invisible': searchType === 'news'
+        }"
+      >
+        Hot
+      </div>
+      <div class="d-flex align-items-center">
         <b-form-tags
           v-model="keywords"
-          class="mr-2 search__input"
+          class="searchForm__input"
           placeholder="請輸入關鍵字"
           duplicate-tag-text="關鍵字已輸入過了"
           invalid-tag-text="請移除無效字元"
           :tag-validator="keywordValidator"
           remove-on-delete
-          @input="getNewsList"
+          @input="getResults({ searchType: 'news'})"
         >
           <template #add-button-text>
             <SearchIcon />
           </template>
         </b-form-tags>
         <b-dropdown
+          :disabled="!results"
           lazy
           dropleft
           no-caret
-          class="ml-2"
+          class="searchForm__news-filter"
         >
           <template #button-content>
             <FilterIcon />
           </template>
-          <template #default="{ hide }">
-            <b-dropdown-form @submit.prevent="getNewsList(); hide();">
-              <div>
-                聲量
-                <b-input v-model.number="volumeMin" type="number" />-<b-input v-model.number="volumeMax" type="number" />
-              </div>
-              <div>
-                爆發力
-                <b-input v-model.number="powerMin" type="number" />-<b-input v-model.number="powerMax" type="number" />
-              </div>
-              <div>
-                立場
-                <b-checkbox-group v-model="positions" :options="positionOption" buttons />
-              </div>
-              <div>
-                管道
-                <b-checkbox-group v-model="channels" :options="channelOption" buttons />
-              </div>
-              <b-button type="submit">
-                送出
-              </b-button>
-            </b-dropdown-form>
-          </template>
+          <b-dropdown-form @submit.prevent="getResults({ searchType: 'news'})">
+            <div class="d-flex align-items-center">
+              聲量
+              <b-input v-model.number="volumeMin" type="number" />
+              -
+              <b-input v-model.number="volumeMax" type="number" />
+            </div>
+            <div class="d-flex align-items-center">
+              爆發力
+              <b-input v-model.number="powerMin" type="number" />
+              -
+              <b-input v-model.number="powerMax" type="number" />
+            </div>
+            <div>
+              立場
+              <b-checkbox-group v-model="positions" :options="positionOption" buttons />
+            </div>
+            <div>
+              管道
+              <b-checkbox-group v-model="channels" :options="channelOption" buttons />
+            </div>
+            <b-button type="submit">
+              送出
+            </b-button>
+          </b-dropdown-form>
         </b-dropdown>
         <div>
           <HotIcon />
@@ -55,14 +65,15 @@
       </div>
     </div>
     <b-table
+      fixed
       responsive
       show-empty
       :fields="fields"
-      :items="news"
-      :busy="!news"
+      :items="results"
+      :busy="!results"
       sort-desc
       sort-direction="desc"
-      @sort-changed="sortChanged"
+      @sort-changed="getResults"
     >
       <template #table-busy>
         loading
@@ -75,7 +86,7 @@
       v-model="page"
       :total-rows="total"
       :per-page="limit"
-      @input="getNewsList"
+      @input="getResults"
     />
   </b-container>
 </template>
@@ -95,7 +106,7 @@ export default {
   data () {
     return {
       total: 0,
-      news: null,
+      results: null,
       positionOption: null,
       channelOption: null,
       fields: [
@@ -145,13 +156,23 @@ export default {
       orderby: '',
       ordertype: '',
       page: 1,
-      limit: 20
+      limit: 20,
+      searchType: 'news'
+    }
+  },
+  computed: {
+    api () {
+      switch (this.searchType) {
+        case 'news':
+          return 'getResultsWithNews'
+      }
+      return 'getResultsWithHotNews'
     }
   },
   mounted () {
     this.getChannelItems()
     this.getPostionItems()
-    this.getNewsList()
+    this[this.api]()
   },
   methods: {
     ...mapActions('api/keyword', ['getNews']),
@@ -170,8 +191,8 @@ export default {
         this.positionOption = !data.error ? data : []
       })
     },
-    getNewsList () {
-      this.news = null
+    getResultsWithNews () {
+      this.results = null
       const option = {
         pageNo: this.page,
         pageSize: this.limit,
@@ -194,37 +215,106 @@ export default {
         option.powerMax = this.powerMax
       }
       this.getNews(option).then((data) => {
-        this.news = !data.error ? data.News : []
+        this.results = !data.error ? data.News : []
         this.total = !data.error ? data.totalNews : 0
       })
     },
-    keywordValidator (keyword) {
-      return !keyword.includes(',')
-    },
-    sortChanged ({ sortBy, sortDesc }) {
+    getResults ({ sortBy, sortDesc = true, searchType }) {
       this.ordertype = sortDesc ? 'DESC' : 'ASC'
       this.orderby = sortBy
-      this.getNewsList()
+      if (searchType) {
+        this.searchType = searchType
+      }
+      return this[this.api]()
+    },
+    keywordValidator (keyword) {
+      return !keyword.includes(',')
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  // .search {
-  //   &__input {
-  //     padding: 0;
-  //     min-width: 280px;
-  //     background: rgba(242, 201, 76, 0.15);
-  //     border-radius: 5px;
+  .searchForm {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 950px;
+    margin: 0 auto 9px auto;
 
-  //     ::v-deep {
-  //       input {
-  //         height: 25px;
-  //         font-size: 18px;
-  //         line-height: 21px;
-  //       }
-  //     }
-  //   }
-  // }
+    &__input {
+      margin-right: 5px;
+      background-color: rgba(242, 201, 76, 0.15);
+      padding: 3px 10px;
+      min-width: 280px;
+
+      ::v-deep {
+        .b-form-tags-list {
+          margin: 0 0 -5px 0 !important;
+
+          .b-form-tag {
+            margin: 0 10px 5px 0;
+            background-color: #2F80ED;
+            border: 1px solid #BCD5F5;
+            border-radius: 5px;
+          }
+
+          .b-form-tags-input,
+          .b-form-tags-button {
+            margin: 0 0 5px 0 !important;
+          }
+
+          .b-form-tags-button {
+            padding: 0;
+            border: none;
+            display: flex;
+            align-items: center;
+
+            &:hover,
+            &:focus,
+            &:active,
+            &:active:focus {
+              background: none;
+              outline: none;
+              box-shadow: none;
+            }
+          }
+        }
+      }
+    }
+
+    &__news-filter {
+      margin-right: 8px;
+
+      ::v-deep {
+        .dropdown-toggle {
+          padding: 0;
+          border: none;
+          background-color: transparent;
+
+          &:active,
+          &:focus {
+            background-color: transparent;
+            outline: none;
+            box-shadow: none;
+          }
+        }
+      }
+
+      &.show {
+        ::v-deep {
+          .dropdown-toggle {
+            background-color: transparent;
+
+            &:focus,
+            &:active {
+              background-color: transparent;
+              outline: none;
+              box-shadow: none;
+            }
+          }
+        }
+      }
+    }
+  }
 </style>
